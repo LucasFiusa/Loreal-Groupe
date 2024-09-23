f3deae819d04ff8e7865c738a88ae62455094e6d
Lucas Fiusa <lucasfiusa100@gmail.com>
2024-09-23 15:49:41 -0300

    atualização

--- script.js
+++ script.js
@@ -46,44 +46,6 @@ const btnNext = document.getElementById('avancar_btn');
 
 let currentSlide = 0;
 
-function hideSlider() {
-    slider.forEach(item => item.classList.remove('on'))
-  }
-  
-  
-  function showSlider() {
-    slider[currentSlide].classList.add('on')
-  }
-  
-  
-  function nextSlider() {
-    hideSlider()
-    if(currentSlide === slider.length -1) {
-      currentSlide = 0
-    } else {
-      currentSlide++
-    }
-    showSlider()
-  }
-  
-  
-  function prevSlider() {
-    hideSlider()
-    if(currentSlide === 0) {
-      currentSlide = slider.length -1
-    } else {
-      currentSlide--
-    }
-    showSlider()
-  }
-  
-  
-  btnNext.addEventListener('click', nextSlider)
-  btnPrev.addEventListener('click', prevSlider)
-  
-  
-  showSlider();
-  
 // FIM CARROSSEL
 
 // const swiper = new Swiper('.swiper' , {
